1c7f44df1eca9dc4afe24e98db653d21462226ee
Anoop555777 <ajbisht99@gmail.com>
2022-09-14 23:47:43 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 import "./categories.style.scss";
 import Home from "./routes/home/home.component";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
 
-      <Route path="/" element={<Navigate replace to="/home" />} />
+        {/* <Route path="shop" element={<Shop />} /> */}
+      </Route>
     </Routes>
   );
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assert/logo.svg";
+import "./navigation.styles.scss";
 const Navigation = () => {
-  return <header className="header"></header>;
+  return (
+    <Fragment>
+      <header className="header">
+        <Link to="/" className="header__logo--link">
+          <Logo className="header__logo" />
+        </Link>
+        <nav className="main__nav">
+          <ul className="main__nav--lists">
+            <li>
+              <Link className="main__nav--link" to="/shop">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link className="main__nav--link" to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link className="main__nav--link" to="/sign-in">
+                SignIn
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
